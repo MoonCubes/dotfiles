c8a57dd4b56a26ffe41d7e1a29178d47c58b06b2
MoonCubes <ole.alpha.hotel@gmail.com>
2024-10-05 23:02:34 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir -p ~/.config
+mkdir -p ~/.local/bin
+mkdir -p ~/.local/share/backgrounds/unsplash
+
+cp -r ./.config/betterlockscreen/  ~/.config/
+cp -r ./.config/nvim/              ~/.config/
+cp -r ./.config/picom/             ~/.config/
+cp -r ./.config/qtile              ~/.config/
+cp -r ./.config/rofi/              ~/.config/
+cp ./.local/share/backgrounds/unsplash/johannes-plenio-hvrpOmuMrAI-unsplash.jpg ~/.local/share/backgrounds/unsplash/
+
+cp ./.local/bin/timer ~/.local/bin
+
